9e1f32120c78d21878cd58300e4f89892e8063d1
john-kelly <johnkellytemena@gmail.com>
2025-03-13 16:39:19 +0800

    Add blur div

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,5 +2,11 @@ import "../styles/home.css";
 
 export default () => {
     let page = document.createElement("div");
+    
+    let blurDiv = document.createElement("div");
+    blurDiv.id = "blur";
+
+    page.appendChild(blurDiv);
+
     return page;
 };
