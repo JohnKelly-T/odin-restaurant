47a5367f1deec59135867be294859302e7707e0d
john-kelly <johnkellytemena@gmail.com>
2025-03-13 19:02:19 +0800

    Import menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/reset.css";
 import "./styles/global.css";
 import createHomePage from "./pages/home";
+import createMenuPage from "./pages/menu";
 
 let contentDiv = document.querySelector("#content");
 let homePage = createHomePage();
