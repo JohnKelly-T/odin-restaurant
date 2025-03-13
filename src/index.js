18954bc17a88d3d0cac4445d091f4e7844ee0c5f
john-kelly <johnkellytemena@gmail.com>
2025-03-13 19:05:03 +0800

    Add home and menu nav event listeners

--- src/index.js
+++ src/index.js
@@ -6,4 +6,17 @@ import createMenuPage from "./pages/menu";
 let contentDiv = document.querySelector("#content");
 let homePage = createHomePage();
 
+let homeNav = document.querySelector("#home");
+let menuNav = document.querySelector("#menu");
+
+homeNav.addEventListener("click", () => {
+    contentDiv.textContent = '';
+    contentDiv.appendChild(createHomePage());
+});
+
+menuNav.addEventListener("click", () => {
+    contentDiv.textContent = '';
+    contentDiv.appendChild(createMenuPage());
+});
+
 contentDiv.appendChild(homePage);
