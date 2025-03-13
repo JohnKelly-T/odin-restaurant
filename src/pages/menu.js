6014ee8afda48241ee2377f69c21041d3f03d892
john-kelly <johnkellytemena@gmail.com>
2025-03-13 19:37:48 +0800

    Add menu page content basic structure

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,6 +2,26 @@ import "../styles/menu.css";
 
 export default () => {
     let page = document.createElement("div");
+    page.id = "menu-content";
+
+    let imagesContainer = document.createElement("div");
+    imagesContainer.id = "menu-images-container";
+
+    let image1 = document.createElement("div");
+    image1.id = "menu-image-1";
+
+    let image2 = document.createElement("div");
+    image2.id = "menu-image-2";
+
+    imagesContainer.appendChild(image1);
+    imagesContainer.appendChild(image2);
+
+    page.appendChild(imagesContainer);
+
+    let menuContainer = document.createElement("div");
+    menuContainer.id = "menu-container";
+
+    page.appendChild(menuContainer);
 
     document.body.className = '';
     document.body.classList.add("body-menu");
