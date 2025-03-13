3f2c520bc92bd9e82e082cb838b252bd5d3c4b1f
john-kelly <johnkellytemena@gmail.com>
2025-03-13 16:18:39 +0800

    Append homepage to DOM inside content div

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./styles/reset.css";
 import "./styles/global.css";
 import createHomePage from "./pages/home";
+
+let contentDiv = document.querySelector("#content");
+let homePage = createHomePage();
+contentDiv.appendChild(homePage);
