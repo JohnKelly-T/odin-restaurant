f6bcef84a8474d397957139b5c735e003388e27c
john-kelly <johnkellytemena@gmail.com>
2025-03-13 19:11:04 +0800

    Export page element

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,1 +1,10 @@
 import "../styles/menu.css";
+
+export default () => {
+    let page = document.createElement("div");
+
+    document.body.className = '';
+    document.body.classList.add("body-menu");
+    
+    return page;
+}
