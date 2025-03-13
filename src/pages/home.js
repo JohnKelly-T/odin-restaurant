eb33bb82b297715129ce08cceccd1bde5e33c4a6
john-kelly <johnkellytemena@gmail.com>
2025-03-13 16:55:54 +0800

    Add welcome message

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,5 +8,20 @@ export default () => {
 
     page.appendChild(blurDiv);
 
+    let welcomeMessageContainer = document.createElement("div");
+    welcomeMessageContainer.id = "welcome-message-container";
+
+    let welcomeMessage = document.createElement("div");
+    welcomeMessage.id = "welcome-message";
+    welcomeMessage.textContent = "Welcome to";
+    welcomeMessageContainer.appendChild(welcomeMessage);
+
+    let restaurantName = document.createElement("div");
+    restaurantName.id = "restaurant-name";
+    restaurantName.textContent = "Odin Dining";
+    welcomeMessageContainer.appendChild(restaurantName);
+
+    page.appendChild(welcomeMessageContainer);
+    
     return page;
 };
