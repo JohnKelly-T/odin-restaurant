756c0f44464cd178463f498d7042d44778786abf
john-kelly <johnkellytemena@gmail.com>
2025-03-14 13:42:33 +0800

    Add about.js file

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import "../styles/about.css";
+
+export default () => {
+    let page = document.createElement("div");
+
+    document.body.className = '';
+    document.body.classList.add("body-about");
+    
+    return page;
+}
